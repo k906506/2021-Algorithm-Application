657582a88080aab99a724d77439083ed531fd9a6
k906506 <k906506@naver.com>
2021-04-24 14:46:38 +0900

    BFS, DFS

---
+++ 6 Week/exp/201701971_고도현.py
@@ -0,0 +1,70 @@
+import sys
+from collections import deque
+
+def groupIsland(map_list, src_x, src_y, n, groupId):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+    queue.append((src_x, src_y))
+
+    map_list[src_x][src_y] = groupId
+
+    while queue:
+        src_x, src_y = queue.popleft()
+        for i in range(4):
+            nx = src_x + dx[i]
+            ny = src_y + dy[i]
+            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                if map_list[nx][ny] == 1:
+                    map_list[nx][ny] = groupId
+                    queue.append((nx, ny))
+    return map_list
+
+def findDistance(map_list, island, dist, id, n):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while island:
+        src_x, src_y = island.popleft()
+        for i in range(4):
+            nx = src_x + dx[i]
+            ny = src_y + dy[i]
+            if nx >= 0 and nx < n and ny >= 0 and ny < n:
+                if map_list[nx][ny] == 0 and dist[nx][ny] == -1: # 바다고 아직 간척사업이 되지 않은 경우
+                    dist[nx][ny] = dist[src_x][src_y] + 1 # 해당 지점을 메꾼다
+                    island.append((nx, ny))
+                if map_list[nx][ny] < 0 and map_list[nx][ny] != id: # 다른 섬을 만난 경우
+                    return dist[src_x][src_y]
+
+def main():
+    n = int(input())
+
+    map_list = [[] for _ in range(n)]
+
+    for i in range(n):
+        map_list[i] = list(map(int, sys.stdin.readline().split()))
+
+    groupId = -1
+    for i in range(n):
+        for j in range(n):
+            if map_list[i][j] == 1:
+                map_list = groupIsland(map_list, i, j, n, groupId)
+                groupId -= 1
+
+    # 섬 별로 grouping
+
+    ans = sys.maxsize
+    for id in range(-1, groupId, -1):
+        island = deque()
+        dist = [[-1 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if map_list[i][j] == id:
+                    island.append((i, j))
+                    dist[i][j] = 0 # 0인 부분은 땅, -1인 부분은 바다
+        ans = min(ans, findDistance(map_list, island, dist, id, n))
+
+    print(ans)
+
+if __name__ == "__main__":
+    main()
