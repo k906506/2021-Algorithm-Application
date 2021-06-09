1fb29b2db4e34fb53ad98c7ebdbd97b4b778d74d
k906506 <k906506@naver.com>
2021-06-09 18:22:16 +0900

    퀵소트

--- Final_Test/3.py
+++ Final_Test/3.py
@@ -1,4 +1,4 @@
-def selectSort(numbers): # 순회하면서 가장 작은 원소랑 위치 변경
+def selectSort(numbers):  # 순회하면서 가장 작은 원소랑 위치 변경
     print("[선택 정렬] 정렬 전 : ", str(numbers))
 
     for i in range(len(numbers)):
@@ -12,9 +12,10 @@ def selectSort(numbers): # 순회하면서 가장 작은 원소랑 위치 변경
         numbers[min_index] = temp
 
     print("[선택 정렬] 정렬 후 : ", str(numbers))
+    print("")
 
 
-def insertSort(numbers): # 순회하면서 이전 원소와 크기 비교
+def insertSort(numbers):  # 순회하면서 이전 원소와 크기 비교
     print("[삽입 정렬] 정렬 전 : ", str(numbers))
 
     for i in range(1, len(numbers)):
@@ -23,10 +24,32 @@ def insertSort(numbers): # 순회하면서 이전 원소와 크기 비교
                 temp = numbers[j - 1]
                 numbers[j - 1] = numbers[j]
                 numbers[j] = temp
-            else: # 이전 값이 더 작으면 순회 필요 없음.
+            else:  # 이전 값이 더 작으면 순회 필요 없음.
                 break
 
     print("[삽입 정렬] 정렬 후 : ", str(numbers))
+    print("")
+
+
+def quickSort(numbers, start, finish):
+    if start >= finish:
+        return
+    pivot = start
+    left = start + 1
+    right = finish
+    while left <= right:
+        while left <= finish and numbers[left] <= numbers[pivot]:
+            left += 1
+        while right >= start and numbers[right] >= numbers[pivot]:
+            right -= 1
+        if left > right:
+            numbers[right], numbers[pivot] = numbers[pivot], numbers[right]
+        else:
+            numbers[left], numbers[right] = numbers[right], numbers[left]
+
+    quickSort(numbers, start, right - 1)
+    quickSort(numbers, right + 1, finish)
+
 
 
 def main():
@@ -36,6 +59,9 @@ def main():
 
     another_numbers = numbers.copy()
     insertSort(another_numbers)
+
+    another_numbers = numbers.copy()
+    quickSort(another_numbers, 0, len(numbers) - 1)
 
 
 if __name__ == "__main__":
